779a47c884a0405505653f8b5aa80fae9b69588d
Amin <asaa47721@gmail.com>
2020-03-24 23:44:05 +0430

    define generate view

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+import string
+import random
+from .forms import OptionsForm
 
-# Create your views here.
+
+def generate(request):
+    form = OptionsForm(request.GET)
+    password = ''
+    if form.is_valid():
+        cd = form.cleaned_data
+        length = cd.get('length', 14)
+        uppercase = cd['uppercase']
+        special_chars = cd['special_chars']
+
+        chars = string.ascii_lowercase + string.digits
+
+        if uppercase:
+            chars += string.ascii_uppercase
+        
+        if special_chars:
+            chars += "!@#$%^&*()_+=-[]{}\\/.,:;"
+        
+        password = ''.join(random.choices(chars, k=length))
+
+    context = {
+        'form': form,
+        'password': password
+    }
+
+
+    return render(request, 'generator/generate.html', context)
